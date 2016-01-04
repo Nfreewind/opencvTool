20cc09a6644361c3f9b8b370602aaf240b6afc8f
shengyu <shengyu7697@gmail.com>
2016-01-04 19:24:35 +0800

    add recordVideo

---
+++ recordVideo/recordVideo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+void help()
+{
+    cout << "\nThis program is used to record video\n"
+        "Usage:\n"
+        "  Record video:      press r\n"
+        "  Exit the program:  press q or ESC\n";
+}
+
+int main(int argc, char** argv)
+{
+    Mat image;
+    string filename;
+    VideoCapture cap;
+    VideoWriter outputVideo;
+    int start_record = 0;
+    int fourcc = 828601953; // avc1
+    Size size;
+
+    help();
+    cap.open(0);
+
+    filename = "output.avi";
+    size = Size((int) cap.get(CAP_PROP_FRAME_WIDTH), (int) cap.get(CAP_PROP_FRAME_HEIGHT));
+    outputVideo.open(filename, fourcc, cap.get(CAP_PROP_FPS), size, true);
+    namedWindow("view", WINDOW_AUTOSIZE);
+    while (1) {
+        cap.read(image);
+        imshow("view", image);
+
+        int key;
+        key = waitKey(10);
+        if (key == 'q' || key == 27)
+            break;
+        if (key == 'r') {
+            start_record ^= 1;
+        }
+
+        if (start_record) {
+            outputVideo.write(image);
+            cout << "write to " << filename << " ..." << endl;
+        }
+    }
+
+    return 0;
+}
