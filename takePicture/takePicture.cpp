fd9395120e9e9327b23f7f73b1bfc80ee641fddb
shengyu <shengyu7697@gmail.com>
2016-01-04 19:21:56 +0800

    update takePicture

--- takePicture/takePicture.cpp
+++ takePicture/takePicture.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "opencv2/opencv.hpp"
 
 using namespace std;
 using namespace cv;
 
+void help()
+{
+    cout << "\nThis program is used to take picture\n"
+        "Usage:\n"
+        "  Save image:        press s\n"
+        "  Exit the program:  press q or ESC\n";
+}
+
+void saveImage(Mat image)
+{
+    static int count = 1;
+    string filename;
+
+    stringstream ss;
+    ss << count;
+    filename = "image" + ss.str() + ".jpg";
+    imwrite(filename, image);
+    cout << "write to " << filename << " ..." << endl;
+    count++;
+}
+
 int main(int argc, char** argv)
 {
     Mat image;
-    string filename;
     VideoCapture cap;
 
+    help();
     cap.open(0);
+    namedWindow("view", WINDOW_AUTOSIZE);
 
-    while(1) {
+    while (1) {
         cap.read(image);
-
-        namedWindow("view", WINDOW_AUTOSIZE);
         imshow("view", image);
 
         int key;
@@ -23,9 +45,7 @@ int main(int argc, char** argv)
         if (key == 'q' || key == 27)
             break;
         if (key == 's') {
-            filename = "saved.jpg";
-            imwrite(filename, image);
-            cout << "write to " << filename << " ..." << endl;
+            saveImage(image);
         }
     }
 
