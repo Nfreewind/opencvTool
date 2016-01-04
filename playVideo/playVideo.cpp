f1c99523f219eec817dcdf63cd46dbee766574b2
shengyu <shengyu7697@gmail.com>
2016-01-04 19:21:16 +0800

    update playVideo

--- playVideo/playVideo.cpp
+++ playVideo/playVideo.cpp
@@ -4,22 +4,57 @@
 using namespace std;
 using namespace cv;
 
+void help()
+{
+    cout << "\nThis program is used to play video\n"
+        "Usage:\n"
+        "./playVideo [video_name]\n"
+        "  Save image:        press s\n"
+        "  Exit the program:  press q or ESC\n";
+}
+
+void saveImage(Mat image)
+{
+    static int count = 1;
+    string filename;
+
+    stringstream ss;
+    ss << count;
+    filename = "image" + ss.str() + ".jpg";
+    imwrite(filename, image);
+    cout << "write to " << filename << " ..." << endl;
+    count++;
+}
+
 int main(int argc, char** argv)
 {
     Mat image;
     string filename;
     VideoCapture cap;
 
-    if (argc < 2)
+    if (argc != 2) {
+        help();
         return 1;
-    
+    }
+
     filename = argv[1];
     cap.open(filename);
+    int ex = static_cast<int>(cap.get(CAP_PROP_FOURCC));
+    char EXT[] = {(char)(ex & 0XFF),
+        (char)((ex & 0XFF00) >> 8),
+        (char)((ex & 0XFF0000) >> 16),
+        (char)((ex & 0XFF000000) >> 24),
+        0};
+    Size size = Size((int) cap.get(CAP_PROP_FRAME_WIDTH), (int) cap.get(CAP_PROP_FRAME_HEIGHT));
+    cout << "Input resolution: Width=" << size.width << " Height=" << size.height << endl;
+    cout << "Input frame count: " << cap.get(CAP_PROP_FRAME_COUNT) << endl;
+    cout << "Input codec type: " << EXT << endl;
     namedWindow("view", WINDOW_AUTOSIZE);
-    
-    while(1) {
-        cap.read(image);
 
+    while (1) {
+        cap.read(image);
+        if (image.empty())
+            break;
         imshow("view", image);
 
         int key;
@@ -27,9 +62,7 @@ int main(int argc, char** argv)
         if (key == 'q' || key == 27)
             break;
         if (key == 's') {
-            filename = "saved.jpg";
-            imwrite(filename, image);
-            cout << "write to " << filename << " ..." << endl;
+            saveImage(image);
         }
     }
 
