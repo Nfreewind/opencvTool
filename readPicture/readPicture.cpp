dad331e453a32305f77412c8935b6464ed03adf6
shengyu <shengyu7697@gmail.com>
2016-01-04 19:21:34 +0800

    update readPicture

--- readPicture/readPicture.cpp
+++ readPicture/readPicture.cpp
@@ -4,14 +4,23 @@
 using namespace std;
 using namespace cv;
 
+void help()
+{
+    cout << "\nThis program is used to read picture\n"
+        "Usage:\n"
+        "./readPicture [image_name]\n";
+}
+
 int main(int argc, char** argv)
 {
     Mat image;
-    string filename("lena.jpg");
+    string filename;
 
-    if (argc > 1) {
-        filename = argv[1];
+    if (argc != 2) {
+        help();
+        return 1;
     }
+    filename = argv[1];
     image = imread(filename, IMREAD_COLOR);
 
     namedWindow("view", WINDOW_AUTOSIZE);
